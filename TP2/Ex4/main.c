a91c688d17247c2c7469a41ff21d798c54dd752e
Mangawn <51322328+Mangawn@users.noreply.github.com>
2020-09-29 00:30:57 +0200

    Main de l'exo 4

--- TP2/Ex4/main.c
+++ TP2/Ex4/main.c
@@ -5,15 +5,32 @@
 #include "graphlib.h"
 
 int main() {
-    srand (time (NULL));
+  srand (time (NULL));
+  
+  gr_inits_w(500,500,"TP2 - Exercice 2");
 
+  set_black();
+  overwrite_gr(100,50,"Fonction 1 : dessineCarre(225,275,50)");
+  dessineCarre(225,275,50);
+  cliquer();
 
-    gr_inits_w(500,500,"test");
+  clear_screen();
+  set_black();
+  overwrite_gr(100,50,"Fonction 2 : dessineCarreDiagonale(225,275,50)");
+  dessineCarreDiagonale(225,275,50);
+  cliquer();
 
-    dessineMosaiqueAvecSouris(50,8,5);
+  clear_screen();
+  set_black();
+  overwrite_gr(100,50,"Fonction 3 : dessineMosaique(50,225,275,8,5)   ");
+  dessineMosaique(50,100,400,6,5);
+  cliquer();
 
-    cliquer();
-
+  clear_screen();
+  set_black();
+  overwrite_gr(100,50,"Fonction 4 : dessineMosaiqueAvecSouris(50,8,5)  ");
+  dessineMosaiqueAvecSouris(50,8,5);
+  cliquer();
 
   return 0;
 }
