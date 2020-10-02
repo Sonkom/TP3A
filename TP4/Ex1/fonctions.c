7bda2273c79a5d76df4cb021424b35c95e126c76
sonkom <smohamet1999@gmail.com>
2020-10-02 12:49:10 +0200

    Fonctions terminés / Tests + debugging incoming

--- TP4/Ex1/fonctions.c
+++ TP4/Ex1/fonctions.c
@@ -19,5 +19,43 @@ int lireDonnees(char nomFichier[], int tab[])
     index++;
     success++;
   }
+  fclose(fichier);
+  return success;
+}
+
+void afficherTableau (int tab[], int nb)
+{
+  for (int i = 0; i < nb; i++) printf("tab[%d] = %d\n",i, tab[i]);
+}
+
+void swap_int(int *a, int *b) //Pour plus de clarté, une fonction dédié à l'inversage d'éléments sera dédiés
+{
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
+
+void triABulles(int tab[], int nb)
+{
+  for (int i = 0; i < nb; i++)
+    for (int j = nb - i; j > 0; j++)
+      if (tab[j] < tab[j-1]) swap((tab+j), (tab+j-1));
+}
+
+void enregistrerDonnees(char nomFichier[], int tab[], int nb)
+{
+  FILE *fichier;
+
+  fichier = fopen(nomFichier, "w");
+  if(fichier == NULL) {
+    perror("Probleme ouverture fichier monFichier.txt");
+    exit(1);
+  }
+
+  for (int index = 0; index < nb; index++)
+    fprintf(fichier, "%d ",tab[index]);
+
+  fclose(fichier);
   return 1;
+
 }
--- TP4/Ex1/fonctions.h
+++ TP4/Ex1/fonctions.h
@@ -3,4 +3,11 @@
 
 #define MAXT 1024
 
+/*---- FONCTIONS ----*/
 int lireDonnees(char nomFichier[], int tab[]);
+void afficherTableau (int tab[], int nb);
+void triABulles(int tab[], int nb);
+void enregistrerDonnees(char nomFichier[], int tab[], int nb);
+void swap_int(int *a, int *b); //Fonction supplémentaire
+
+/*---- TESTS ----*/
